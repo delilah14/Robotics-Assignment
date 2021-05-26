b8017c259f574cbdd4a25f362b93e3c1094ef0fa
delilah14 <dilluxdes@gmail.com>
2021-05-26 21:36:39 +0300

    (feature) Subscriber Node Added.

--- src/arm_gazebo/src/controller.cpp
+++ src/arm_gazebo/src/controller.cpp
@@ -4,6 +4,11 @@
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 #include <iostream>
+#include <thread>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+#include <std_msgs/Float32.h>
 using namespace std;
 
 namespace gazebo
@@ -41,8 +46,61 @@ namespace gazebo
 			// simulation iteration.
 			this->updateConnection = event::Events::ConnectWorldUpdateBegin(
 				std::bind(&ModelPush::OnUpdate, this));
-		}
 
+
+							// Initialize ros, if it has not already bee initialized.
+			if (!ros::isInitialized())
+			{
+			int argc = 0;
+			char **argv = NULL;
+			ros::init(argc, argv, "gazebo_client",
+				ros::init_options::NoSigintHandler);
+			}
+
+			// Create our ROS node. This acts in a similar manner to
+			// the Gazebo node
+			this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+			// Create a named topic, and subscribe to it.
+			ros::SubscribeOptions so =
+			ros::SubscribeOptions::create<std_msgs::Float32>(
+				"/" + this->model->GetName() + "/vel_cmd",
+				1,
+				boost::bind(&ModelPush::OnRosMsg, this, _1),
+				ros::VoidPtr(), &this->rosQueue);
+			this->rosSub = this->rosNode->subscribe(so);
+
+			// Spin up the queue helper thread.
+			this->rosQueueThread =
+			std::thread(std::bind(&ModelPush::QueueThread, this));
+		}
+		
+
+			public: void SetVelocity(const double &_vel)
+				{
+				// Set the joint's target velocity.
+				 std::string name = this->model->GetJoint("arm2_arm3_joint")->GetScopedName();
+				this->jointController->SetVelocityTarget(name, 10.0);
+				
+				}
+		
+
+
+			public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
+			{
+				this->SetVelocity(1.0);
+			
+			}
+
+			/// \brief ROS helper function that processes messages
+			private: void QueueThread()
+			{
+			static const double timeout = 0.01;
+			while (this->rosNode->ok())
+			{
+				this->rosQueue.callAvailable(ros::WallDuration(timeout));
+			}
+}
 		// Called by the world update start event
 	public:
 		void OnUpdate()
@@ -101,6 +159,19 @@ namespace gazebo
 		// // 	// PID object
 	private:
 		common::PID pid;
+	/// \brief A node use for ROS transport
+	private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+	/// \brief A ROS subscriber
+	private: ros::Subscriber rosSub;
+
+	/// \brief A ROS callbackqueue that helps process messages
+	private: ros::CallbackQueue rosQueue;
+
+	/// \brief A thread the keeps running the rosQueue
+	private: std::thread rosQueueThread;
+
+
 	};
 
 	// Register this plugin with the simulator
